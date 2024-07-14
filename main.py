8b9f0818f3b2271458c1cc1d9005ab7b724cf1b2
schwelmz <moritz.schweller@web.de>
2024-07-14 20:27:11 +0200

    1st working version

--- main.py
+++ main.py
@@ -1,36 +1,38 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import settings
+import time
+import visualization as vis
 
 # read parameters
 alpha_N, alpha_L, k_p, n_N, n_L, K_N, K_L, gamma_N, gamma_L, D_N, D_L = settings.read_parameters()
 settings.print_settings()
 
 xstart = 0
-xend = 1
+xend = 15
 ystart = 0
-yend = 1
+yend = 15
 tstart = 0
-tend = 1
-ht = 1e-2
-hx = 1e-1
-hy = 1e-1
-Nx = int((xend-xstart)/hx)
-Ny = int((yend-ystart)/hy)
-Nt = int((tend-tstart)/ht)
+tend = 10
+hx = 1.3e-1
+hy = 1.3e-1
+ht = 5e-5
+Nx = int((xend-xstart)/hx + 1)
+Ny = int((yend-ystart)/hy + 1)
+Nt = int((tend-tstart)/ht + 1)
 xs = np.linspace(xstart,xend,Nx)
 ys = np.linspace(ystart,yend,Ny)
 N = np.zeros((Nt,Nx,Ny))
 L = np.zeros((Nt,Nx,Ny))
 
+print(f"hx={hx:.2e}, hy={hy:.2e}, ht={ht:.2e}")
+
 def hill_equation(N,L):
     nenner = pow(N,n_N) + pow(K_N * (1 + pow(L/K_L,n_L)), n_N)
     zahler = pow(N,n_N)
-    # if zahler != 0:
-    #     print("true")
     return zahler/nenner
 
-def initialize_matrix(rows, cols, init_val=1, percentage=0.05):
+def initialize_matrix(rows, cols, init_val=100, percentage=0.05):
     # Step 1: Create a 2D array filled with zeros
     array = np.zeros((rows, cols), dtype=int)
     
@@ -50,32 +52,44 @@ def initialize_matrix(rows, cols, init_val=1, percentage=0.05):
     return array
 
 #initial condition
-# N[0,:,:] = initialize_matrix(Nx,Ny)
-# L[0,:,:] = initialize_matrix(Nx,Ny)
-m = Nx//2
-N[0,m-1:m+1,m-1:m+1] = 0.01
-L[0,m-1:m+1,m-1:m+1] = 0.01
+N[0,:,:] = initialize_matrix(Nx,Ny)
+L[0,:,:] = initialize_matrix(Nx,Ny)
+# m = Nx//2
+# N[0,m,m] = 1
+# L[0,m,m] = 1
 
-fig, axs = plt.subplots(1,2)
+fig, axs = plt.subplots(1,2,figsize=(12,5))
 #main loop
+tik = time.time()
 for n in range(0,Nt-1):
-    print(n+1,"/",Nt)
-    for i in range(1,Nx-1):
-        for j in range(1,Ny-1):
-            dN_dxx = (N[n,i-1,j]-2*N[n,i,j]+N[n,i+1,j])/(hx**2)
-            dN_dyy = (N[n,i,j-1]-2*N[n,i,j]+N[n,i,j+1])/(hy**2)
-            dL_dxx = (L[n,i-1,j]-2*L[n,i,j]+L[n,i+1,j])/(hx**2)
-            dL_dyy = (L[n,i,j-1]-2*L[n,i,j]+L[n,i,j+1])/(hy**2)
-            # print("dNdx",dN_dxx,"dNdy",dN_dyy)
-            # print("dLdx",dL_dxx,"dLdy",dL_dyy)
-            print(i,j,N[n,i,j],L[n,i,j])
+    print(f"\rtime step {n+1}/{Nt}",end=" ",flush=True)
+    for i in range(0,Nx):
+        for j in range(0,Ny):
+            #calculate the diffusion term using 2nd order finite differneces (with periodic BC)
+            dN_dxx = (N[n,(i-1)%Nx,j]-2*N[n,i,j]+N[n,(i+1)%Nx,j])/(hx**2)
+            dN_dyy = (N[n,i,(j-1)%Ny]-2*N[n,i,j]+N[n,i,(j+1)%Ny])/(hy**2)
+            dL_dxx = (L[n,(i-1)%Nx,j]-2*L[n,i,j]+L[n,(i+1)%Nx,j])/(hx**2)
+            dL_dyy = (L[n,i,(j-1)%Ny]-2*L[n,i,j]+L[n,i,(j+1)%Ny])/(hy**2)
+            #calculate the hill equation term
             f1 = alpha_N*hill_equation(N[n,i,j],L[n,i,j])
             f2 = alpha_L* hill_equation(N[n,i,j],L[n,i,j])
-            # print(f1,f2)
-            N[n+1,i,j] = N[n,i,j] + ht*(f1 - gamma_L*L[n,i,j] + D_N*(dN_dxx+dN_dyy))
-            L[n+1,i,j] = L[n,i,j] + ht*(f2 - gamma_N*N[n,i,j] + D_L*(dL_dxx+dL_dyy))
-    plt.clf()
-    plt.imshow(N[n,:,:])
-    plt.colorbar()
-    # plt.pause(0.5)
-    plt.show()
+            #calculate Nodal and Lefty at the new time step
+            N[n+1,i,j] = N[n,i,j] + ht*(f1 - gamma_N*N[n,i,j] + D_N*(dN_dxx+dN_dyy))
+            L[n+1,i,j] = L[n,i,j] + ht*(f2 - gamma_L*L[n,i,j] + D_L*(dL_dxx+dL_dyy))
+    if n%1000==0:
+        np.save(f"out/Nodal_quicksave_{ht}_{hx}_{hy}_{tend}_{xend}_{yend}_{alpha_N}_{alpha_L}.npy",N)
+        np.save(f"out/Lefty_quicksave_{ht}_{hx}_{hy}_{tend}_{xend}_{yend}_{alpha_N}_{alpha_L}.npy",L)
+    #liveplot
+    if False:
+        tok = time.time()
+        print(f"\ndone! time taken: {(tok-tik)/60:.1f}min")
+        vis.heatmap(fig,axs,N,L,n,[xstart,xend,ystart,yend],ht)
+        plt.pause(0.5)
+        cb0.remove()
+        cb1.remove()
+        axs[0].cla()
+        axs[1].cla()
+
+#save data
+np.save(f"out/Nodal_{ht}_{hx}_{hy}_{tend}_{xend}_{yend}_{alpha_N}_{alpha_L}.npy",N)
+np.save(f"out/Lefty_{ht}_{hx}_{hy}_{tend}_{xend}_{yend}_{alpha_N}_{alpha_L}.npy",L)
---
+++ visualization.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import imageio
+
+def heatmap(fig,axs,N,L,timestep,extent,ht):
+    n = timestep
+    fig.suptitle(f"t={n*ht:.5f}min")
+    #ax0
+    im0 = axs[0].imshow(N[n,:,:])#,extent,origin="lower")
+    axs[0].set_title(f"Nodal")
+    axs[0].set_xlabel(r"domain width [$\mu m$]")
+    axs[0].set_ylabel(r"domain height [$\mu m$]")
+    cb0 = fig.colorbar(im0, ax=axs[0])
+    cb0.set_label("Nodal [nM]")
+    #ax1
+    im1 = axs[1].imshow(L[n,:,:])#,extent,origin="lower")
+    axs[1].set_title(f"Lefty")
+    axs[1].set_xlabel(r"domain width [$\mu m$]")
+    axs[1].set_ylabel(r"domain height [$\mu m$]")
+    cb1 = fig.colorbar(im1, ax=axs[1])
+    cb1.set_label("Lefty [nM]")
+
+
+def create_gif(N,L,extent,Nt,ht,skips):
+    fig,axs = plt.subplots(1,2,figsize=(12,5))
+    for tdx in range(0,Nt,skips):
+        print("\r",tdx,end="",flush=True)
+        fig.suptitle(f"t={tdx*ht:.5f}min")
+        #ax0
+        im0 = axs[0].imshow(N[tdx,:,:])#,extent,origin="lower")
+        axs[0].set_title(f"Nodal")
+        axs[0].set_xlabel(r"domain width [$\mu m$]")
+        axs[0].set_ylabel(r"domain height [$\mu m$]")
+        cb0 = fig.colorbar(im0, ax=axs[0])
+        cb0.set_label("Nodal [nM]")
+        #ax1
+        im1 = axs[1].imshow(L[tdx,:,:])#,extent,origin="lower")
+        axs[1].set_title(f"Lefty")
+        axs[1].set_xlabel(r"domain width [$\mu m$]")
+        axs[1].set_ylabel(r"domain height [$\mu m$]")
+        cb1 = fig.colorbar(im1, ax=axs[1])
+        cb1.set_label("Lefty [nM]")
+        #savefig
+        fig.savefig('plots/heatmap_frame_'+str(tdx)+'.png')
+        cb0.remove()
+        cb1.remove()
+        axs[0].cla()
+        axs[1].cla()
+    # Build GIF
+    print('\n building .gif file')
+    with imageio.get_writer(f'heatmap_{Nt}_{skips}.mp4', fps=20) as writer:
+        for filename in range(0,Nt,skips):
+            image = imageio.v2.imread('plots/heatmap_frame_' + str(filename)+ '.png')
+            writer.append_data(image)
