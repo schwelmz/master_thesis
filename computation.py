ea8085438120ba4e6c4a8f025e1df03c084eb289
schwelmz <moritz.schweller@web.de>
2024-10-21 12:25:50 +0200

    add phase diagram plot to computation

--- computation.py
+++ computation.py
@@ -8,7 +8,7 @@ import shutil
 
 # read parameters
 parameters = settings.read_parameters()
-settings.print_settings(parameters)
+# settings.print_settings(parameters)
 np.random.seed(0)
 
 #read command line arguments
@@ -33,12 +33,9 @@ if setup == "NL":   #Nodal-Lefty
     D_L = float(parameters['D_L'])
     xstart = 0
     xend = 100*np.sqrt(D_N/gamma_N)
-    ystart = 0
-    yend = 100*np.sqrt(D_N/gamma_N)
     tstart = 0
     tend = 100/gamma_N
     Nx = 101
-    Ny = 101
     Nt = int(1e5)
 elif setup == "GM":     #Gierer-Meinhardt
     D_u = float(parameters["D_u"])
@@ -49,12 +46,9 @@ elif setup == "GM":     #Gierer-Meinhardt
     r = float(parameters["r"])
     xstart = 0
     xend = 100
-    ystart = 0
-    yend = 100
     tstart = 0
     tend = 100
     Nx = 200
-    Ny = 200
     Nt = int(1e4)
 elif setup == "NL_dimless":     #dimensionaless Nodal-Lefty
     alpha_N = float(parameters['alpha_N'])
@@ -79,23 +73,18 @@ elif setup == "NL_dimless":     #dimensionaless Nodal-Lefty
     print("d = ",d)
     xstart = 0
     xend = 100
-    ystart = 0
-    yend = 100
     tstart = 0
     tend = 100
     Nx = 101
-    Ny = 101
     Nt = int(1e5)
     dimless=True
 
 #Define the spatial and temporal grid
 hx = (xend-xstart)/(Nx-1)
-hy = (xend-xstart)/(Ny-1)
 ht = (tend-tstart)/(Nt-1)
 xs = np.linspace(xstart,xend,Nx)
-ys = np.linspace(ystart,yend,Ny)
-print(f"hx={hx:.2e}, hy={hy:.2e}, ht={ht:.2e}")
-print(f"Nx={Nx}, Ny={Ny}, Nt={Nt}")
+print(f"hx={hx:.2e}, ht={ht:.2e}")
+print(f"Nx={Nx}, Nt={Nt}")
 
 '''
 return the values of the hill equation as it is in the PDE for different concentrations of Nodal and Lefty
@@ -111,7 +100,8 @@ Input: full (Nx x Ny) matrix U (with boundary entries)
 Output: approximation of the Laplace operator in each inner entry of the matrix ((Nx-2)x(Ny-2))
 '''
 def central_differences(U):
-    return (U[0:-2,1:-1] + U[2:,1:-1] + U[1:-1,0:-2] + U[1:-1,2:] - 4*U[1:-1,1:-1])/(hx**2)
+    # return (U[0:-2,1:-1] + U[2:,1:-1] + U[1:-1,0:-2] + U[1:-1,2:] - 4*U[1:-1,1:-1])/(hx**2)
+    return (U[0:-2] - 2*U[1:-1] + U[2:])/(hx**2)
 
 # def laplacian_2d(arr, dx, dy):
 #     laplacian_x = (np.roll(arr, 1, axis=0) - 2 * arr + np.roll(arr, -1, axis=0)) / dx**2
@@ -126,38 +116,38 @@ options for initialization: "4dots" (1 dot in each quarter of the matrix),
                             "white-noise" (random values sampled from a uniform distribution over [0,1) )
 Output: matrix containing initial values
 '''
-def initialize_matrix(rows, cols, option="white-noise"):
+def initialize_matrix(rows, option="white-noise"):
     init_val = 1
     percentage = 0.001
-    if option == "4dots":
-        array = np.zeros((rows,cols))
-        d_x = cols//4
-        d_y = rows//4
-        array[d_x,d_y] = init_val
-        array[3*d_x,3*d_y] = init_val
-        array[d_x,3*d_y] = init_val
-        array[3*d_x,d_y] = init_val
-        return array
-    elif option == "random-dots":
-        # Step 1: Create a 2D array filled with zeros
-        array = np.zeros((rows, cols), dtype=int)
-        # Step 2: Calculate the number of ones needed
-        total_elements = rows * cols
-        num_ones = int(total_elements * percentage)
-        # Step 3: Randomly select positions to place the ones
-        ones_positions = np.random.choice(total_elements, num_ones, replace=False)
-        # Step 4: Convert 1D positions to 2D indices and place the ones
-        for pos in ones_positions:
-            row_index = pos // cols
-            col_index = pos % cols
-            array[row_index, col_index] = init_val
-        return array
-    elif option == "white-noise":
+    # if option == "4dots":
+    #     array = np.zeros((rows,cols))
+    #     d_x = cols//4
+    #     d_y = rows//4
+    #     array[d_x,d_y] = init_val
+    #     array[3*d_x,3*d_y] = init_val
+    #     array[d_x,3*d_y] = init_val
+    #     array[3*d_x,d_y] = init_val
+    #     return array
+    # elif option == "random-dots":
+    #     # Step 1: Create a 2D array filled with zeros
+    #     array = np.zeros((rows, cols), dtype=int)
+    #     # Step 2: Calculate the number of ones needed
+    #     total_elements = rows * cols
+    #     num_ones = int(total_elements * percentage)
+    #     # Step 3: Randomly select positions to place the ones
+    #     ones_positions = np.random.choice(total_elements, num_ones, replace=False)
+    #     # Step 4: Convert 1D positions to 2D indices and place the ones
+    #     for pos in ones_positions:
+    #         row_index = pos // cols
+    #         col_index = pos % cols
+    #         array[row_index, col_index] = init_val
+    #     return array
+    if option == "white-noise":
         # Generate a 2D array with white noise using a normal distribution
         # low = 0.0
         # high = 0.01
         # return np.random.uniform(low, high, (rows, cols))
-        return np.random.rand(rows,cols)
+        return np.random.rand(rows)
 
 '''
 One time step for the Nodal-Lefty model:
@@ -170,11 +160,11 @@ def NodalLefty_step(N_old, L_old):
     # Lap_N = laplacian_2d(N_old,hx,hy)
     # Lap_L = laplacian_2d(L_old,hx,hy)
     #calculate the hill equation term
-    hill_term = hill_equation(N_old[1:-1,1:-1],L_old[1:-1,1:-1])
+    hill_term = hill_equation(N_old[1:-1],L_old[1:-1])
     # hill_term = hill_equation(N_old,L_old)
     #calculate Nodal and Lefty at the new time step using the explicit euler method
-    N_new = N_old[1:-1,1:-1] + ht*(alpha_N*hill_term - gamma_N*N_old[1:-1,1:-1] + D_N*Lap_N)
-    L_new = L_old[1:-1,1:-1] + ht*(alpha_L*hill_term - gamma_L*L_old[1:-1,1:-1] + D_L*Lap_L)
+    N_new = N_old[1:-1] + ht*(alpha_N*hill_term - gamma_N*N_old[1:-1] + D_N*Lap_N)
+    L_new = L_old[1:-1] + ht*(alpha_L*hill_term - gamma_L*L_old[1:-1] + D_L*Lap_L)
     # N_new = N_old + ht*(alpha_N*hill_term - gamma_N*N_old + D_N*Lap_N)
     # L_new = L_old + ht*(alpha_L*hill_term - gamma_L*L_old + D_L*Lap_L)
     return N_new, L_new
@@ -184,8 +174,8 @@ def NodalLefty_dimless_step(U_old, V_old):
     Lap_u = central_differences(U_old)
     Lap_v = central_differences(V_old)
     #calculate the hill equation term
-    u_old = U_old[1:-1,1:-1]
-    v_old = V_old[1:-1,1:-1]
+    u_old = U_old[1:-1]
+    v_old = V_old[1:-1]
     hill_term = pow(u_old,n_N)/(pow(u_old,n_N) + pow((1+pow(v_old,n_L)),n_N))
     #calculate Nodal and Lefty at the new time step using the explicit euler method
     u_new = u_old + ht*(alpha_N_ * hill_term - u_old + Lap_u)
@@ -216,35 +206,33 @@ Solve the PDE for a given model
 def solver(model_step):
     #main loop
     tik = time.time()
-    A_new = np.zeros((Nx,Ny))
-    B_new = np.zeros((Nx,Ny))
+    A_new = np.zeros(Nx)
+    B_new = np.zeros(Nx)
     A_old = A_init
     B_old = B_init
     for n in range(0,Nt-1):
         print(f"\rtime step {n+1}/{Nt}",end=" ",flush=True)
         #update timestep
-        A_new[1:-1,1:-1], B_new[1:-1,1:-1] = model_step(A_old, B_old)
+        A_new[1:-1], B_new[1:-1] = model_step(A_old, B_old)
         # A_new, B_new = model_step(A_old, B_old)
         #set Neumann boundary values
-        A_new[0,:] = A_new[1,:]     #left
-        B_new[0,:] = B_new[1,:]
-        A_new[:,0] = A_new[:,1]     #bottom
-        B_new[:,0] = B_new[:,1]
-        A_new[-1,:] = A_new[-2,:]   #right
-        B_new[-1,:] = B_new[-2,:]
-        A_new[:,-1] = A_new[:,-2]   #top
-        B_new[:,-1] = B_new[:,-2]
+        A_new[0] = A_new[1]     #left
+        B_new[0] = B_new[1]
+        A_new[-1] = A_new[-2]   #right
+        B_new[-1] = B_new[-2]
         #save plots
         if videomode:
             if n%frameskips == 0 or n in[1,2,3,4,5,10,40,80,150]:
                 #save fig
                 fig, axs = plt.subplots(1,2,figsize=(12,5))
-                img = vis.heatmap(fig,axs,A_new,B_new,n,[xstart,xend,ystart,yend],tstart+ht*n, dimless=dimless)
-                fig.savefig(f"out/{outdir}/plots/heatmap_{n}")
+                # img = vis.heatmap(fig,axs,A_new,B_new,n,[xstart,xend,ystart,yend],tstart+ht*n, dimless=dimless)
+                axs[0].plot(xs,A_new)
+                axs[1].plot(xs,B_new)
+                fig.savefig(f"out/{outdir}/plots/lineplot_{n}")
                 plt.close()
                 #save data
-                np.save(f"out/{outdir}/data/A_{ht}_{hx}_{hy}_{tend}_{xend}_{yend}.npy",A_new)
-                np.save(f"out/{outdir}/data/B_{ht}_{hx}_{hy}_{tend}_{xend}_{yend}.npy",B_new)
+                np.save(f"out/{outdir}/data/A_{ht}_{hx}_{tend}_{xend}.npy",A_new)
+                np.save(f"out/{outdir}/data/B_{ht}_{hx}_{tend}_{xend}_.npy",B_new)
 
         #set new state as old
         A_old = A_new
@@ -262,7 +250,7 @@ if args.input is None:
         init = args.initialization
     else:
         init = "white-noise"
-    init_mat = initialize_matrix(Nx,Ny, init)     #specify initial condition here
+    init_mat = initialize_matrix(Nx, init)     #specify initial condition here
     A_init = init_mat 
     B_init = init_mat
 else:
@@ -293,6 +281,47 @@ elif setup == "GM":
 elif setup == "NL_dimless":
     A_new, B_new = solver(NodalLefty_dimless_step)
 
+if np.max(A_new) > 2*np.min(A_new):
+    print("Condition for pattern formation fulfilled!")
+    val_diff = np.max(A_new) - np.min(A_new)
+    print(f"val_diff = {val_diff}")
+else:
+    print("Condition for pattern formation NOT fulfilled!")
+    val_diff = np.max(A_new) - np.min(A_new)
+    print(f"val_diff = {val_diff}")
+
+#check for pattern formation
+# N = 2
+# max_val = 10
+# phase_diagram = np.zeros((N,N))
+# vals = np.linspace(0,max_val,N)
+# vals = [6,10]
+# for i in range(N):
+#     for j in range(N):
+#         alpha_N = vals[i]
+#         alpha_L = vals[j]
+#         print(alpha_N,alpha_L)
+#         if setup == "NL_dimless":
+#             alpha_N_ = alpha_N/(gamma_N*K_N)
+#             alpha_L_ = alpha_L/(gamma_N*K_L)
+#             # print(f"alpha_N_ = {alpha_N_}, alpha_L_ = {alpha_L_}")
+#         if setup == "NL":
+#             A_new, B_new = solver(NodalLefty_step)
+#         elif setup == "NL_dimless":
+#             A_new, B_new = solver(NodalLefty_dimless_step)
+#         plt.plot(xs,A_new)
+#         plt.ylim(0,np.max(A_new))
+#         plt.show()
+#         val_diff = np.max(A_new) - np.min(A_new)
+#         print(f"val_diff = {val_diff}")
+#         phase_diagram[i,j] = val_diff
+# plt.imshow(phase_diagram, extent=[0,max_val,0,max_val],origin="lower")
+# plt.xlabel(r"$\alpha_N$")
+# plt.ylabel(r"$\alpha_L$")
+# cb = plt.colorbar()
+# cb.set_label(r"$\max(N)-\min(N)$")
+# plt.show()
+
 #save data of last time step
-np.save(f"out/{outdir}/data/A_{ht}_{hx}_{hy}_{tend}_{xend}_{yend}.npy",A_new)
-np.save(f"out/{outdir}/data/B_{ht}_{hx}_{hy}_{tend}_{xend}_{yend}.npy",B_new)
+np.save(f"out/{outdir}/data/A_{ht}_{hx}_{tend}_{xend}.npy",A_new)
+np.save(f"out/{outdir}/data/B_{ht}_{hx}_{tend}_{xend}.npy",B_new)
